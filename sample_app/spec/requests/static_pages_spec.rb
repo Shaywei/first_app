66dcd6f0d17d014ac26e31a4fc46157723ec8165
Shay Weiss <another.shay.weiss@gmail.com>
2013-05-01 20:28:37 +0000

    fixed tests to use root_path for home

--- sample_app/spec/requests/static_pages_spec.rb
+++ sample_app/spec/requests/static_pages_spec.rb
@@ -7,18 +7,18 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit home_path
+      visit root_path
       page.should have_content('Sample App')
     end
 
     it "should have the right title" do
-      visit home_path
+      visit root_path
       page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
-      visit home_path
+      visit root_path
       page.should_not have_selector('title', :text => '| Home')
     end  
 
