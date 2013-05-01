8ce30c99c8e6c19eacf02432d11ceee4b42e4368
Shay Weiss <another.shay.weiss@gmail.com>
2013-05-01 09:05:52 +0000

    adding Procfile and requirements.txt

---
+++ first_app_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f5e3acc9e07d4b984d159f86f6972032ade63786c7b2596399f593cb948cb8ad7941b653b5655b6243f10dc6c2bf04b215af84ed97741ae2d1edbb749cfbe5db'
