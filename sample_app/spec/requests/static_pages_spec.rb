cf7767c7383192cc2c971c2366af53cb315fab92
Shay Weiss <shay.weiss@algowiz.com>
2013-05-01 23:44:54 +0300

    refactoring tests to be pretty

--- sample_app/spec/requests/static_pages_spec.rb
+++ sample_app/spec/requests/static_pages_spec.rb
@@ -3,69 +3,39 @@ require 'spec_helper'
 describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject {page}
 
   describe "Home page" do
-
-    it "should have the content 'Sample App'" do
-      visit home_path
-      page.should have_content('Sample App')
-    end
-
-    it "should have the right title" do
-      visit home_path
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      visit home_path
-      page.should_not have_selector('title', :text => '| Home')
-    end  
+    before { visit root_path }
+    
+    it { should have_content('Sample App') }
+    it { should have_selector('title', :text => "Ruby on Rails Tutorial Sample App") }
+    it { should_not have_selector 'title', text: '| Home' }
 
   end
 
   describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
-
-    it "should have the right title" do
-      visit help_path
-      page.should have_selector('title',
-                        :text => "#{base_title} | Help")
-    end
+    before { visit help_path }
+    
+    it { should have_content('Help') }
+    it { should have_selector('title', :text => "#{base_title} | Help") }
 
   end
 
   describe "About page" do
-
-    it "should have the content 'About Us'" do
-      visit about_path
-      page.should have_content('About Us')
-    end
-
-    it "should have the right title" do
-      visit about_path
-      page.should have_selector('title',
-                        :text => "#{base_title} | About")
-    end  
+    before { visit about_path }
+    
+    it { should have_content('About Us') }
+    it { should have_selector('title', :text => "#{base_title} | About") }
 
   end
 
   describe "Contact page" do
 
-    it "should have the content 'Contact Us'" do
-      visit contact_path
-      page.should have_content('Contact Us')
-    end
+    before { visit contact_path }
 
-    it "should have the right title" do
-      visit contact_path
-      page.should have_selector('title',
-                        :text => "#{base_title} | Contact")
-    end  
+    it { should have_content('Contact Us') }
+    it { should have_selector('title', :text => "#{base_title} | Contact") }
       
   end
 
