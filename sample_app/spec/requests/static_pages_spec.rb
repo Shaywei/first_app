f3cebe0bef11fd880bae121558ca048e7b81c896
Shay Weiss <shay.weiss@algowiz.com>
2013-05-01 17:03:56 +0300

    Added full_title() to ApplicationHelper, application.html.erb now uses it. Added test: Home page should not have a custom "| Home" title (should fail)

--- sample_app/spec/requests/static_pages_spec.rb
+++ sample_app/spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-  
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -14,8 +14,14 @@ describe "Static pages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "#{base_title} | Home")
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+  end    
 
   end
 
